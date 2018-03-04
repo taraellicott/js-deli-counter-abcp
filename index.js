f8b976960d844ce84c541d7df2f6e19b9a2ad634
Tara Ellicott <taraellicott@gmail.com>
2018-03-04 23:07:29 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+var katzDeli = [];
+
+function takeANumber(line, name) {
+  for (var i = 0; i < line.length; i++) {
+  
+    console.log(`Welcome ${name}. You are number ${i-1]}.`);
+  } 
+}
+
+function nowServing( katzDeliLine) {
+return katzDeliLine.shift()
+}
+
+function currentLine() {
+  // returns the current line
+}
